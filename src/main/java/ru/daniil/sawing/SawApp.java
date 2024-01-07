e632020b28310beee30a988b33761292b683e08e
t26x0 <panovd379@gmail.com>
2024-01-07 20:38:01 +0500

    feat: added method for spend validate

--- src/main/java/ru/daniil/sawing/SawApp.java
+++ src/main/java/ru/daniil/sawing/SawApp.java
@@ -4,7 +4,6 @@ import java.util.*;
 
 public class SawApp {
 
-    final Scanner input = new Scanner(System.in);
     private Map<String, List<Integer>> allPersonAndTheirSpend = new HashMap<>();
     private final String GENERAL_PERSON = "generalSpend";
 
@@ -13,24 +12,29 @@ public class SawApp {
     }
 
     public static void main(String[] args) {
+        final Scanner input = new Scanner(System.in);
         String answer;
         SawApp sawApp = new SawApp();
+
         while (true) {
             System.out.println("All options:");
-            System.out.println("   . Add new person");
-            System.out.println("   . Add new spend");
-            System.out.println("   . Add new spend for person");
-            System.out.println("   . What everyone has to pay");
-            System.out.println("   . Show all person");
-            System.out.println("   . Show all expenses for person");
-            answer = sawApp.input.nextLine();
+            System.out.println("  1. Add new person");
+            System.out.println("  2. Add new general spend");
+            System.out.println("  3. Add new spend for person");
+            System.out.println("  4. What everyone has to pay");
+            System.out.println("  5. Show all persons");
+            System.out.println("  6. Show all expenses");
+            System.out.println("  7. Show all expenses for person");
 
+            answer = input.nextLine();
             switch (answer) {
                 case "1" -> {
-
+                    System.out.println("Enter the name of the new participant:");
+                    sawApp.addNewPerson(input.nextLine());
                 }
                 case "2" -> {
-
+                    System.out.println("Enter new spend");
+                    sawApp.addGeneralSpend(Integer.parseInt(input.nextLine()));
                 }
                 case "3" -> {
 
@@ -41,9 +45,13 @@ public class SawApp {
                 case "5" -> {
 
                 }
-                default -> {
-                    System.out.println("You made a mistake when entering data");
+                case "6" -> {
+
                 }
+                case "7" -> {
+
+                }
+                default -> System.out.println("You made a mistake when entering data");
             }
         }
     }
@@ -55,20 +63,19 @@ public class SawApp {
         allPersonAndTheirSpend.put(personName, new ArrayList<>());
     }
 
-    public List<String> getAllPerson() {
+    public void showAllPerson() {
         List<String> allPersons = new ArrayList<>(allPersonAndTheirSpend.keySet());
         allPersons.remove(GENERAL_PERSON);
-        return allPersons;
+        System.out.println(allPersons);
     }
 
     public void addGeneralSpend(int newSpend) {
+        isValidSpend(newSpend);
         allPersonAndTheirSpend.get(GENERAL_PERSON).add(newSpend);
     }
 
     public void addSpend(String personName, int newSpend) {
-        if (newSpend <= 0) {
-            throw new IllegalArgumentException("Spending can't be negative");
-        }
+        isValidSpend(newSpend);
         allPersonAndTheirSpend.get(personName).add(newSpend);
         addGeneralSpend(newSpend);
     }
@@ -80,5 +87,11 @@ public class SawApp {
             sumAllSpends += integer;
         }
         return (double) sumAllSpends / (allPersonAndTheirSpend.size() - 1);
+    }
+
+    private static void isValidSpend(int newSpend) {
+        if (newSpend <= 0) {
+            throw new IllegalArgumentException("Spending can't be negative");
+        }
     }
 }
