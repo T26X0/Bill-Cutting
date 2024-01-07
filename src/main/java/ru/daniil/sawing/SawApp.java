e6b5969baba433b7e2425f0b2a854e510fe77168
t26x0 <panovd379@gmail.com>
2024-01-07 20:22:20 +0500

    refactor: changed entry ppoint

--- src/main/java/ru/daniil/sawing/SawApp.java
+++ src/main/java/ru/daniil/sawing/SawApp.java
@@ -13,53 +13,6 @@ public class SawApp {
     }
 
     public static void main(String[] args) {
-        SawApp sawApp = new SawApp();
-        sawApp.addNewPerson("Daniil");
-        sawApp.addNewPerson("Nikita");
-        sawApp.addGeneralSpend(100);
-        sawApp.addSpend("Daniil", 100);
-        System.out.println(sawApp.getAllPerson());
-        System.out.println(sawApp.divideAmongEveryone());
-    }
-
-    public void addNewPerson(String personName) {
-        if (allPersonAndTheirSpend.containsKey(personName)) {
-            throw new IllegalArgumentException("Person + {" + personName + "} already exist");
-        }
-        allPersonAndTheirSpend.put(personName, new ArrayList<>());
-    }
-
-    public List<String> getAllPerson() {
-        List<String> allPersons = new ArrayList<>(allPersonAndTheirSpend.keySet());
-        allPersons.remove(GENERAL_PERSON);
-        return allPersons;
-    }
-
-    public void addGeneralSpend(int newSpend) {
-        allPersonAndTheirSpend.get(GENERAL_PERSON).add(newSpend);
-    }
-
-    public void addSpend(String personName, int newSpend) {
-        if (newSpend <= 0) {
-            throw new IllegalArgumentException("Spending can't be negative");
-        }
-        allPersonAndTheirSpend.get(personName).add(newSpend);
-        addGeneralSpend(newSpend);
-    }
-
-    public double divideAmongEveryone() {
-        int sumAllSpends = 0;
-        List<Integer> allSpends = new ArrayList<>(allPersonAndTheirSpend.get(GENERAL_PERSON));
-        for (Integer integer : allSpends) {
-            sumAllSpends += integer;
-        }
-        return (double) sumAllSpends / (allPersonAndTheirSpend.size() - 1);
-    }
-}
-
-class Tests {
-
-    public static void main(String[] args) {
         String answer;
         SawApp sawApp = new SawApp();
         while (true) {
@@ -95,16 +48,37 @@ class Tests {
         }
     }
 
-    private static void testForTheRightResult() {
-        SawApp sawApp = new SawApp();
-        sawApp.addNewPerson("Daniil");
-        sawApp.addNewPerson("Nikita");
-        sawApp.addNewPerson("Maxim");
+    public void addNewPerson(String personName) {
+        if (allPersonAndTheirSpend.containsKey(personName)) {
+            throw new IllegalArgumentException("Person + {" + personName + "} already exist");
+        }
+        allPersonAndTheirSpend.put(personName, new ArrayList<>());
+    }
 
-        sawApp.addSpend("Nikita", 100);
-        sawApp.addSpend("Daniil", 100);
-        sawApp.addGeneralSpend(100);
-        int expectedResult = 100;
-        assert sawApp.divideAmongEveryone() == expectedResult;
+    public List<String> getAllPerson() {
+        List<String> allPersons = new ArrayList<>(allPersonAndTheirSpend.keySet());
+        allPersons.remove(GENERAL_PERSON);
+        return allPersons;
+    }
+
+    public void addGeneralSpend(int newSpend) {
+        allPersonAndTheirSpend.get(GENERAL_PERSON).add(newSpend);
+    }
+
+    public void addSpend(String personName, int newSpend) {
+        if (newSpend <= 0) {
+            throw new IllegalArgumentException("Spending can't be negative");
+        }
+        allPersonAndTheirSpend.get(personName).add(newSpend);
+        addGeneralSpend(newSpend);
+    }
+
+    public double divideAmongEveryone() {
+        int sumAllSpends = 0;
+        List<Integer> allSpends = new ArrayList<>(allPersonAndTheirSpend.get(GENERAL_PERSON));
+        for (Integer integer : allSpends) {
+            sumAllSpends += integer;
+        }
+        return (double) sumAllSpends / (allPersonAndTheirSpend.size() - 1);
     }
 }
